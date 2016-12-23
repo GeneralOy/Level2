2c9a44c08e2b925dfdf229a1971539b8b34c3a4a
rylr <league@WTS9.attlocal.net>
2016-12-22 16:40:27 -0800

    a
    s
    d
    f

--- src/Frogger/FroggerObject.java
+++ src/Frogger/FroggerObject.java
@@ -24,7 +24,6 @@ public class FroggerObject {
 			frogImage = ImageIO.read(this.getClass().getResourceAsStream("Frogger.png"));
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
